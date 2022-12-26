5315164c3097715b9f3f91f7c3898519abcea9cd
정민지 <83000829+jungminji0215@users.noreply.github.com>
2022-12-26 17:21:59 +0900

    타겟넘버

---
+++ programmers/타켓넘버.java
@@ -0,0 +1,22 @@
+public class Solution {
+    int answer = 0;
+
+    public int solution(int[] numbers, int target){
+
+        dfs(numbers,0,0, target);
+        return answer;
+    }
+
+    public void dfs(int[] numbers, int depth, int sum, int target){
+        if(depth == numbers.length){
+            if(sum == target){
+                answer += 1;
+            }
+            return;
+        } else {
+
+            dfs(numbers, depth + 1, sum + numbers[depth], target);
+            dfs(numbers, depth + 1, sum - numbers[depth], target);
+        }
+    }
+}
