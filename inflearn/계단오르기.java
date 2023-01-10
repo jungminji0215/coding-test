710a6fb9747c47df59459a7e8e83e2247c0cb08f
정민지 <83000829+jungminji0215@users.noreply.github.com>
2023-01-11 06:29:48 +0900

    계단오르기

---
+++ inflearn/계단오르기.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[] dy;
+    public static int solution(int n){
+        dy[1] = 1;
+        dy[2] = 2;
+
+        for(int i = 3; i <= n; i++){
+            dy[i] = dy[i-2] + dy[i-1];
+        }
+        return dy[n];
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        dy = new int[n+1];
+        System.out.println(solution(n));
+    }
+}
