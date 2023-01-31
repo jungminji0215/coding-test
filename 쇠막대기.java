92e3730f121c014567695a8f4d5626f0b4ffc25f
정민지 <83000829+jungminji0215@users.noreply.github.com>
2023-01-31 16:44:45 +0900

    쇠막대기

---
+++ 쇠막대기.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+class Main {
+    public static int solution(String str) {
+        int answer = 0;
+
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == '('){
+                stack.push('(');
+            }else {
+                stack.pop();
+                if(str.charAt(i-1) == '('){
+                    answer += stack.size();
+                }else {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("()(((()())(())()))(())"));
+    }
+}
