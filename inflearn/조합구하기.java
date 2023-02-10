97202b07a2e1e3672b180d1efdb7f22d486bb658
정민지 <83000829+jungminji0215@users.noreply.github.com>
2023-02-10 19:35:59 +0900

    Update 조합구하기.java

--- inflearn/조합구하기.java
+++ inflearn/조합구하기.java
@@ -3,28 +3,29 @@ import java.util.*;
 // 조합 구하는 법 그냥 외우삼
 class Main {
     static int[] combi;
-    static int N, M;
+    static int n, m;
 
-    static void dfs(int level, int start) {
-        if(level == M){
+    static void DFS(int level, int start) {
+        if(level == m){
             for(int x : combi) System.out.print(x + " ");
             System.out.println();
 
-        } else {
-            for(int i = start; i <= N; i++){
+        }
+        else {
+            for(int i = start; i <= n; i++){
                 combi[level] = i;
-                dfs(level+1, start+1);
+                DFS(level+1, i+1);
             }
         }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        N = 4;
-        M = 2;
+        n = 4;
+        m = 2;
 
-        combi = new int[M];
+        combi = new int[m];
 
-        dfs(0,1); // Level, start
+        DFS(0,1); // Level, start
     }
 }
