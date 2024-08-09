e3f945a757550ff0a317c127f3194c5eef155fd8
jungminji <wjdalswl0215@gmail.com>
2024-08-09 10:12:54 +0900

    x 만큼 간격이 있는 n 개의 숫자 다른사람 풀이

--- 12954.js
+++ 12954.js
@@ -1,11 +1,28 @@
+// function solution(x, n) {
+//   var answer = [];
+//   let num = x;
+
+//   for (let i = 0; i < n; i++) {
+//     answer.push(num);
+//     num += x;
+//   }
+
+//   return answer;
+// }
+
 function solution(x, n) {
-  var answer = [];
-  let num = x;
+  let test = Array(n).fill(x);
+  let test2 = test.map((v, i) => {
+    console.log("==========");
+    console.log(v);
+    console.log(i);
+    return (i + 1) * v;
+  });
 
-  for (let i = 0; i < n; i++) {
-    answer.push(num);
-    num += x;
-  }
+  console.log(test);
+  console.log(test2);
 
-  return answer;
+  return test2;
 }
+
+solution(2, 5);
