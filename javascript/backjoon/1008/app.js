0da10459a644683107aa0bc4fc55527398d0dcbe
jungminji <wjdalswl0215@gmail.com>
2024-11-26 15:16:58 +0900

    [백준] 10998/1001/1008

---
+++ javascript/backjoon/1008/app.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+const stringArr = fs.readFileSync(filePath).toString().split(" ");
+const numberSrr = stringArr.map((item) => Number(item));
+
+function solution(arr) {
+  return arr[0] / arr[1];
+}
+
+console.log(solution(numberSrr));
