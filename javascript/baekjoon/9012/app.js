f0a00e2114b77c5a6b148a9794d35779d4fa7e98
jungminji <wjdalswl0215@gmail.com>
2024-12-04 20:01:14 +0900

    [백준] 9012 | 괄호

---
+++ javascript/baekjoon/9012/app.js
@@ -0,0 +1,97 @@
+/**
+갯수로 판단하면 ())(() 이런거에서 실패함
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().split("\n");
+
+function solution(input) {
+    for (let i = 1; i <= input[0]; i++) {
+      let count = 0;
+      for (let j = 0; j < input[i].length; j++) {
+        if (input[i][j] === "(") {
+          count = count + 1;
+        } else {
+          count = count - 1;
+      }
+    }
+    
+    count === 0 ? console.log("YES") : console.log("NO");
+  }
+}
+
+solution(input);
+*/
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().split("\n");
+
+class Stack {
+  #array;
+
+  constructor(array = []) {
+    this.#array = array;
+  }
+
+  push(value) {
+    return this.#array.push(value);
+  }
+
+  pop() {
+    if (this.#array.length === 0) {
+      return -1;
+    }
+    return this.#array.pop();
+  }
+
+  size() {
+    return this.#array.length;
+  }
+
+  empty() {
+    // 스택이 비어있으면 1, 아니면 0 출력
+    return this.#array.length === 0 ? 1 : 0;
+  }
+
+  // 스택의 가장 위에 있는 정수를 출력한다.
+  // 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+  top() {
+    if (this.#array.length === 0) {
+      return -1;
+    } else {
+      return this.#array[this.#array.length - 1];
+    }
+  }
+
+  toString() {
+    return this.#array;
+  }
+}
+
+function solution(input) {
+  let result = [];
+
+  for (let i = 1; i <= input[0]; i++) {
+    const stack = new Stack();
+    let isYes = true;
+    for (let j = 0; j < input[i].length; j++) {
+      if (input[i][j] === "(") {
+        stack.push(input[i][j]);
+      } else {
+        if (stack.pop() === -1) {
+          isYes = false;
+          break;
+        }
+      }
+    }
+
+    // 만약 실패가 아니면 YES
+    if (stack.size() !== 0) {
+      isYes = false;
+    }
+    isYes ? result.push("YES") : result.push("NO");
+  }
+  return result;
+}
+console.log(solution(input).join("\n"));
