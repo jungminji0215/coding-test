5a178d08335e24c4bc5c27edec9f94f6f627808d
jungminji <wjdalswl0215@gmail.com>
2025-01-10 02:45:22 +0900

    [백준] 3085 | 사탕 게임 | 다시 풀어봄

--- javascript/baekjoon/silver/3085/app.js
+++ javascript/baekjoon/silver/3085/app.js
@@ -3,100 +3,69 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-function solution(input) {
-  let maxCount = 0;
+const checkMaxCount = (candy, maxCount) => {
+  // 행체크
+  for (let row of candy) {
+    let count = 1;
 
+    for (let i = 0; i < candy.length - 1; i++) {
+      if (row[i] === row[i + 1]) {
+        count++;
+      } else {
+        maxCount = Math.max(maxCount, count);
+        count = 1;
+      }
+      maxCount = Math.max(maxCount, count);
+    }
+  }
+
+  // 열체크
+  for (let col = 0; col < candy[0].length - 1; col++) {
+    let count = 1;
+    for (let row = 0; row < candy.length - 1; row++) {
+      if (candy[row][col] === candy[row + 1][col]) {
+        count++;
+      } else {
+        maxCount = Math.max(maxCount, count);
+        count = 1;
+      }
+    }
+    maxCount = Math.max(maxCount, count);
+  }
+
+  return maxCount;
+};
+
+function solution(input) {
   const N = Number(input[0]);
-  const candy = input
-    .filter((_, index) => index !== 0)
-    .map((el) => el.split(""));
+  const candy = input.filter((_, index) => index !== 0).map((c) => c.split(""));
+
+  let maxCount = 0;
 
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < N; j++) {
-      // 다를 경우 바꾸기
+      // 오른쪽
       if (j + 1 < N && candy[i][j] !== candy[i][j + 1]) {
+        // 바꾼다
         const target = candy[i][j];
         candy[i][j] = candy[i][j + 1];
         candy[i][j + 1] = target;
 
-        // row 최대 길이 탐색
-        for (let row of candy) {
-          let count = 1;
+        maxCount = checkMaxCount(candy, maxCount);
 
-          for (let i = 0; i < row.length - 1; i++) {
-            if (row[i] === row[i + 1]) {
-              count++;
-            } else {
-              maxCount = Math.max(maxCount, count);
-
-              count = 1;
-            }
-            maxCount = Math.max(maxCount, count);
-          }
-        }
-
-        // col 최대 길이 탐색
-        for (let col = 0; col < candy[0].length; col++) {
-          let count = 1;
-
-          for (let row = 1; row < candy.length; row++) {
-            if (candy[row][col] === candy[row - 1][col]) {
-              count++;
-            } else {
-              maxCount = Math.max(maxCount, count);
-              count = 1;
-            }
-          }
-
-          maxCount = Math.max(maxCount, count); // 마지막 연속 체크
-        }
-
-        // 되돌려 주기
+        // 되돌리기
         candy[i][j + 1] = candy[i][j];
         candy[i][j] = target;
       }
-
-      // 아래쪽 변경
-
-      // 다를 경우 바꾸기
+      // 아래쪽
       if (i + 1 < N && candy[i][j] !== candy[i + 1][j]) {
+        // 바꾼다
         const target = candy[i][j];
         candy[i][j] = candy[i + 1][j];
         candy[i + 1][j] = target;
 
-        // row 최대 길이 탐색
-        for (let row of candy) {
-          let count = 1;
-
-          for (let i = 0; i < row.length - 1; i++) {
-            if (row[i] === row[i + 1]) {
-              count++;
-            } else {
-              maxCount = Math.max(maxCount, count);
-
-              count = 1;
-            }
-            maxCount = Math.max(maxCount, count);
-          }
-        }
-
-        // col 최대 길이 탐색
-        for (let col = 0; col < candy[0].length; col++) {
-          let count = 1;
-
-          for (let row = 1; row < candy.length; row++) {
-            if (candy[row][col] === candy[row - 1][col]) {
-              count++;
-            } else {
-              maxCount = Math.max(maxCount, count);
-              count = 1;
-            }
-          }
-
-          maxCount = Math.max(maxCount, count); // 마지막 연속 체크
-        }
-
-        // 되돌려 주기
+        maxCount = checkMaxCount(candy, maxCount);
+        // 되돌리기
         candy[i + 1][j] = candy[i][j];
         candy[i][j] = target;
       }
