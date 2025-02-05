102ec067e82c071410ec5d6ebba6a966f5b97689
jungminji <wjdalswl0215@gmail.com>
2025-02-06 00:25:34 +0900

    [백준] 15656 | N과 M (7)

---
+++ javascript/baekjoon/silver/15656/app.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+function solution(input) {
+  const [N, M] = input[0].split(" ").map(Number);
+
+  const testCase = input[1]
+    .split(" ")
+    .map(Number)
+    .sort((a, b) => a - b);
+
+  const result = [];
+  const temp = [];
+
+  const 백트래킹 = () => {
+    if (temp.length === M) {
+      result.push([...temp]);
+      return;
+    }
+
+    for (let i = 0; i < N; i++) {
+      temp.push(testCase[i]);
+      백트래킹();
+      temp.pop();
+    }
+  };
+
+  백트래킹();
+
+  return result.map((num) => num.join(" ")).join("\n");
+}
+
+console.log(solution(input));
