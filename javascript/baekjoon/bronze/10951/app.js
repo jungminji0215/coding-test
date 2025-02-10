58977497cc276ae2556a8dea99972811faa3698d
jungminji <wjdalswl0215@gmail.com>
2025-02-10 21:28:47 +0900

    [백준] 10951 | A+B - 4

---
+++ javascript/baekjoon/bronze/10951/app.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+function solution(input) {
+  const result = [];
+
+  for (let i = 0; i < input.length; i++) {
+    const [A, B] = input[i].split(" ").map(Number);
+    result.push(A + B);
+  }
+
+  return result.join("\n");
+}
+
+console.log(solution(input));
