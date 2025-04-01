ef975c5945bc6cab33d46cd9e2b38b6e15039fc8
jungminji <wjdalswl0215@gmail.com>
2025-04-02 00:40:08 +0900

    [백준] 11718 | 그대로 출력하기

---
+++ javascript/baekjoon/bronze/11718/app.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+const input = fs.readFileSync(filePath).toString().trim();
+
+function solution(input) {
+  return input;
+}
+
+console.log(solution(input));
